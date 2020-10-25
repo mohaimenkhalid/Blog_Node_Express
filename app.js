82055059d2c26e9a7496b6e407f64d5e7de8b27e
Abdul Mohaimen <mohaimen707@gmail.com>
2020-10-26 01:03:10 +0600

    templeting for login register

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express')
-const app = express();
+const morgan = require('morgan')
 
-app.get('/', () => {
-    res.json({
-        message : 'Hello Word'
-    })
+const app = express();
+// Setup view Engine
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+//Middleware Array
+const middleware = [
+    morgan('dev'),
+    express.static('public'),
+    express.urlencoded({ extended: true }),
+    express.json()
+]
+app.use(middleware)
+
+app.get('/', (req, res) => {
+    res.render('pages/auth/signup', { title : 'Create a new account'})
 })
 
-const PORT = process.env.PORT
-app.listen(POST, () => {
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
 })
