4bae119a7d048793484d0a7059fc05d39791212c
mohaimen <mohaimen707@gmail.com>
2020-10-29 00:31:40 +0600

    signup validation added

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { body, validationResult } = require('express-validator')
+const User = require('../models/User.js')
 
 const {
         signupGetController,
@@ -7,7 +9,24 @@ const {
         loginPostController,
         logoutController
     } = require('../controllers/authController')
-
+const signupValidation = [
+        body('username')
+            .isLength({min: 2, max: 30}).withMessage('Username must be 2 to 30 chars')
+            .custom(async username => {
+                    let user = await User.findOne({ username });
+                    if (user) {
+                            return Promise.reject('Username already taken.')
+                    }
+            }),
+        body('email')
+            .isEmail().withMessage('Please provide a valid email address.')
+            .custom(async email=>{
+                    let user = await User.findOne({ email })
+                    if(user){
+                         return Promise.reject('Email address already taken')
+                    }
+            })
+]
 router.get('/signup', signupGetController)
 router.post('/signup', signupPostController)
 
